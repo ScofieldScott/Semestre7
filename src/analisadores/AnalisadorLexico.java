632018161ab1566947c7395eaa9f6692f8f21da6
ScofieldScott <107198967+ScofieldScott@users.noreply.github.com>
2023-06-12 16:46:33 -0300

    Add files via upload

---
+++ src/analisadores/AnalisadorLexico.java
@@ -0,0 +1,151 @@
+package analisadores;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import execoes.ExecaoLexica;
+
+public class AnalisadorLexico {
+	private char[] content;
+	private int estado;
+	private int posicao;
+	private int linha;
+	private int coluna;
+	
+	public AnalisadorLexico(String input) {
+		try { 
+			String txtConteudo;
+			txtConteudo = new String(Files.readAllBytes(Paths.get(input)), StandardCharsets.UTF_8);
+			System.out.println("DEBUG --------- ");
+			System.out.println(txtConteudo);
+			System.out.println("-------------");
+			content = txtConteudo.toCharArray();
+			posicao = 0;
+			linha = 0;
+			coluna = 0;
+		}
+		catch(Exception ex) {
+			ex.printStackTrace(); 
+		}
+	}
+	
+	public Token nextToken() {
+		char posAtual;
+		String term = "";
+		Token token;
+		
+		if(isItOff()) {
+			return null;
+		}
+
+		estado = 0;
+		while(true) {
+			posAtual = nextChar();
+			coluna++;
+			
+			switch(estado) {
+			case 0:
+				 if(isChar(posAtual)) {
+					 term += posAtual;
+					 estado = 1;
+				 }
+				 else if(isDigit(posAtual)) {
+					 estado = 2;
+					 term += posAtual;
+				 }
+				 else if (isSpace(posAtual)) {
+					 estado = 0;
+				 }
+				 else if (isOperator(posAtual)){
+					 term += posAtual;
+					 token = new Token();
+					 token.setTipo(Token.TK_OPERADOR);
+					 token.setText(term);
+					 token.setLinha(linha);
+					 token.setColuna(coluna - term.length());
+					 return token;
+				 }
+				 else {
+					  throw new ExecaoLexica("Caracter nao reconhecido."); 
+				 }
+				 break;
+			
+			case 1:
+				if(isChar(posAtual) || isDigit(posAtual)) {
+					term += posAtual;
+					estado = 1;
+				}
+				else if (isSpace(posAtual) || isOperator(posAtual)|| isItOff(posAtual)){
+					if (!isItOff(posAtual))
+						back();
+					token = new Token();
+					token.setTipo(Token.TK_IDENTIFICADOR);
+					token.setLinha(linha);
+					token.setColuna(coluna - term.length());
+					token.setText(term);
+					return token;
+				}
+				else {
+					throw new ExecaoLexica("Simbolo nao reconhecido");
+				}
+				break;
+				
+			case 2:
+				if(isDigit(posAtual) || posAtual == '.') {
+					estado = 2;
+					term += posAtual;
+				}
+				else if (!isChar(posAtual) || isItOff(posAtual)){
+					if(!isItOff(posAtual))
+						back();
+					token = new Token();
+					token.setTipo(Token.TK_NUMERO);
+					token.setText(term);
+					token.setLinha(linha);
+					token.setColuna(coluna - term.length());
+					return token;
+				} else {
+					throw new ExecaoLexica("Numero nao reconhecido");
+				}
+				break;
+			}
+		}
+	}
+	
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+	
+	private boolean isChar(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+	}
+	
+	private boolean isOperator(char c) {
+		return c == '>' || c == '<' || c == '=' || c == '!' || c == '+' || c == '-' || c == '*' || c == '/';
+	}
+	
+	private boolean isSpace(char c) {
+		if (c == '\n' || c == '\r') {
+			linha++;
+			coluna = 0;
+		}
+		return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+	} 
+	
+	private char nextChar () {
+		return content[posicao++];
+	}
+	
+	private boolean isItOff () {
+		return posicao >= content.length;
+	}
+
+	private void back() {
+		posicao --;
+	}
+	
+	private boolean isItOff (char c) {
+		return c == '\0';
+	}
+}
---
+++ src/analisadores/Token.java
@@ -0,0 +1,67 @@
+package analisadores;
+
+public class Token {
+	public static final int TK_IDENTIFICADOR = 0;
+	public static final int TK_NUMERO = 1;
+	public static final int TK_OPERADOR = 2;
+	public static final int TK_PONTUACAO = 3;
+	public static final int TK_ATRIBUICAO = 4;
+	
+	public static final String TK_TEXT[] = {
+		"IDENTIFICADOR", "NUMERO", "OPERADOR", "PONTUACAO", "ATRIBUICAO"
+	};
+	
+	private int tipo;
+	private String text;
+	private int linha;
+	private int coluna;
+	
+	public Token(int tipo, String text) {
+		super();
+		this.tipo = tipo;
+		this.text = text;
+	}
+	
+	public Token() {
+		super();
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+	public String toString() {
+		return "Token [tipo=" + tipo + ", text=" + text + "]";
+	}
+	
+	
+	
+}
