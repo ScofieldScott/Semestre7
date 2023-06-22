a460ff65a95304f26976903e0177373a8bc641d5
ScofieldScott <107198967+ScofieldScott@users.noreply.github.com>
2023-06-22 10:12:40 -0300

    Update Main.java

--- tentando_com_o_professor/src/principal/Main.java
+++ tentando_com_o_professor/src/principal/Main.java
@@ -2,7 +2,7 @@ package principal;
 
 import analisadores.AnalisadorLexico;
 import analisadores.AnalisadorSintatico;
-//import analisadores.Token;
+import analisadores.Token;
 import execoes.ExecaoLexica;
 import execoes.ExecaoSintatica;
 
@@ -13,18 +13,17 @@ public class Main {
 		try {
 			AnalisadorLexico sc = new AnalisadorLexico("input.isi");
 			AnalisadorSintatico parser = new AnalisadorSintatico(sc);
-			/*Token token = null;
+			Token token = null;
 			
 			do {
 				token = sc.nextToken();
 				if (token != null) 
 					System.out.println(token);
 				
-			}while (token != null);*/
-			
+			}while (token != null);
 			parser.Exprecao();
 			System.out.println("Compilado com sucesso!!");
-					
+			
 		}catch(ExecaoLexica ex) {
 			System.out.println("Erro Lexico " + ex.getMessage());
 		}
